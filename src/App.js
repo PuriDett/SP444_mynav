54a54269f04e05ed3f8ee2dc17786e312a7d01aa
Puri <2210517101002@live4.utcc.ac.th>
2023-09-29 10:58:54 +0700

    Add components Contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './components/Homepage';
 import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
 import {Routes,Route,Link} from 'react-router-dom';
 
 function App() {
@@ -10,10 +11,12 @@ function App() {
       <nav className='nav' class="topnav" >
         <Link to="/" className='nav-item'>Homepage</Link>
         <Link to="/about-me" className='nav-item'>About</Link>
+        <Link to="/Contact" className='nav-item'>Contact</Link>
       </nav>
       <Routes>
           <Route path="/" element={<Homepage/>}/>
           <Route path="/about-me" element={<AboutMe/>}/>
+          <Route path="/Contact" element={<Contact/>}/>
       </Routes>
     </div>
   );
