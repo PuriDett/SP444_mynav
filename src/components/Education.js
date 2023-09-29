2d846ba98b7072a1a1e10f71c00afdfef879f0a9
Puri <2210517101002@live4.utcc.ac.th>
2023-09-29 11:37:16 +0700

    npm install react-player

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,9 +1,20 @@
 import utcc1 from "../assets/utcc1.png"
 function Education(){
+    const imgURL = "https://www.utcc.ac.th/wp-content/uploads/2020/05/bg-14.jpeg";
     return(
         <div>
         <h1>Below are my university pictures: </h1>
         <img src={utcc1} width={300} alt="univ pic1"/>
+        <br/>
+        <img src={require("../assets/utcc2.png")} 
+            width={300} alt="univ pic2"/>
+        <br/>
+        <img src={imgURL} width={300} alt="univ pic3"/>
+        <br/>
+        <video width="300" controls>
+            <source src={require("../assets/minions.mp4")} type="Video/mp4" />
+            Sorry , your browser doens't support videos!
+        </video>
         </div>
     );
 }
