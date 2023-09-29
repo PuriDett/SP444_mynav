b560a25d9b7635ace2d2341bb2a8bbfba662662a
Puri <2210517101002@live4.utcc.ac.th>
2023-09-29 11:18:50 +0700

    add image

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,10 @@
+import utcc1 from "../assets/utcc1.png"
 function Education(){
     return(
+        <div>
         <h1>Below are my university pictures: </h1>
+        <img src={utcc1} width={300} alt="univ pic1"/>
+        </div>
     );
 }
 export default Education ;
